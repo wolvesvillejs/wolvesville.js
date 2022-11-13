cd652d681b818652277981bb3523223d8caed188
= <loeizguallar@gmail.com>
2022-11-13 17:21:22 +0100

    chore(scripts): export all constants

--- scripts/generateRequires.js
+++ scripts/generateRequires.js
@@ -5,9 +5,16 @@ const dir = fs.readdirSync('../src', { withFileTypes: true })
   .map(folder => folder.name);
 
 const exported = dir.map(item => {
-  const files = fs.readdirSync('../src/' + item);
+  const files = fs.readdirSync(`../src/${item}`);
   return files.map(file => {
-    return `exports.${file.slice(0, -3)} = require('./${item}/${file.slice(0, -3)}');`
+    var line = `exports.${file.slice(0, -3)} = require('./${item}/${file.slice(0, -3)}');`;
+    if(file === 'Constants.js') {
+      Object.keys(require(`../src/${item}/${file}`)).forEach(constant => {
+        line += `\nexports.${constant} = require('./${item}/${file.slice(0, -3)}').${constant};`;
+      });
+      line += '\n'
+    }
+    return line
   }).join('\n');
 }).join('\n\n');
 
