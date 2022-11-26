aa144770f5a849fe924af8b40ca3ad773bbe72b6
= <loeizguallar@gmail.com>
2022-11-26 15:10:56 +0100

    fix(scripts): wrong required path

--- scripts/generateRequires.js
+++ scripts/generateRequires.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-const dir = fs.readdirSync('../src', { withFileTypes: true })
+const dir = fs.readdirSync('./src', { withFileTypes: true })
   .filter(item => item.isDirectory() && item.name !== 'rest')
   .map(folder => folder.name);
 
 const exported = dir.map(item => {
-  const files = fs.readdirSync(`../src/${item}`);
+  const files = fs.readdirSync(`./src/${item}`);
   return files.map(file => {
     var line = `exports.${file.slice(0, -3)} = require('./${item}/${file.slice(0, -3)}');`;
     if(file === 'Constants.js') {
@@ -18,6 +18,6 @@ const exported = dir.map(item => {
   }).join('\n');
 }).join('\n\n');
 
-fs.writeFileSync('../src/index.js', 
+fs.writeFileSync('./src/index.js', 
   `'use strict';\n\n${exported}`
 );
